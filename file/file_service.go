8671610f8dd247b1538c2ddb92c74799a5fa1122
Cekretik <erofeev.fil9@bk.ru>
2023-11-20 23:46:33 +0600

    add UploadFile func

--- file/file_service.go
+++ file/file_service.go
@@ -1,7 +1,12 @@
 package file
 
 import (
-	"github.com/Cekretik/file-service/tree/main/models"
+	"file-service/models"
+	"fmt"
+	"io"
+	"mime/multipart"
+	"os"
+
 	"gorm.io/gorm"
 )
 
@@ -15,4 +20,45 @@ func NewFileService(db *gorm.DB) *FileService {
 
 func (f *FileService) AutoMigrate() {
 	f.DB.AutoMigrate(&models.File{})
+}
+
+func (f *FileService) UploadFile(fileHeader *multipart.FileHeader) (*models.File, error) {
+	// Создание начальной записи о файле в базе данных
+	fileRecord := &models.File{
+		FileName: fileHeader.Filename,
+		FileSize: fileHeader.Size,
+	}
+	if err := f.DB.Create(fileRecord).Error; err != nil {
+		return nil, err
+	}
+
+	// Открыть файл для чтения
+	file, err := fileHeader.Open()
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	// Путь для сохранения файла, включая ID
+	filePath := fmt.Sprintf("path/to/storage/%d-%s", fileRecord.ID, fileHeader.Filename)
+
+	// Сохранение файла
+	dst, err := os.Create(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer dst.Close()
+
+	// Копирование содержимого файла
+	if _, err = io.Copy(dst, file); err != nil {
+		return nil, err
+	}
+
+	// Обновление записи в базе данных с полным путем к файлу
+	fileRecord.Path = filePath
+	if err := f.DB.Save(fileRecord).Error; err != nil {
+		return nil, err
+	}
+
+	return fileRecord, nil
 }
